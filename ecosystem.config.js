33e37309b19e91f7a5da65b19263c9825f822106
felipemuner <felipe.muner@gmail.com>
2020-02-16 14:56:56 +0700

    last change ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,33 @@
 module.exports = {
-  apps : [{
-    name: 'test app node aws-ec2',
-    script: 'bin/www',
+  apps: [
+    {
+      name: "test app node aws-ec2",
+      script: "/bin/www",
 
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      args: "one two",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'ubuntu',
-      host : '3.92.134.169',
-      ref  : 'origin/master',
-      repo : 'git@github.com:felipe-muner/pm2-deploy-ec2.git',
-      path : '/home/ubu321321ntu',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "3.92.134.169",
+      ref: "origin/master",
+      repo: "git@github.com:felipe-muner/pm2-deploy-ec2.git",
+      path: "/home/ubuntu/pm2-deploy-ec2",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production"
     }
   }
 };
