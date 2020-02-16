c056b326402dea4c39580a60341399b1c263d26d
felipemuner <felipe.muner@gmail.com>
2020-02-16 14:50:41 +0700

    change time to check deploy works well

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.json({ title: "pm2 deploy on aws-ec2" });
+  res.json({ title: "pm2 deploy on aws-ec2 test at 14:50" });
 });
 
 module.exports = router;
